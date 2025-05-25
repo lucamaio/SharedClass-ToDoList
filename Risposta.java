3919af02a7a36b2a0800b3727b2830e0668701fe
lucamaio <lucos.maio@gmail.com>
2025-05-25 15:55:54 +0200

    Parametri

--- Risposta.java
+++ Risposta.java
@@ -1,14 +1,17 @@
 package Shared;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Risposta implements Serializable {
     private String status; // "OK" o "ERRORE"
     private Object messaggio;
+    private HashMap<String, Object> parametri;
 
     public Risposta(String status, Object messaggio) {
         this.status = status;
         this.messaggio = messaggio;
+        this.parametri = new HashMap<>();
     }
 
     public String getStatus() {
@@ -17,5 +20,13 @@ public class Risposta implements Serializable {
 
     public Object getMessaggio() {
         return messaggio;
+    }
+    
+    public void aggiungiParametro(String chiave, Object valore) {
+        parametri.put(chiave, valore);
+    }
+
+    public Object getParametro(String chiave) {
+        return parametri.get(chiave);
     }
 }
